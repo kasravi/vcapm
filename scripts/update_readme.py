2cbfefc45c2d32efb823eb0109ec8a4296fce00e
mortezakasravi <morteza.kasravi@danfoss.com>
2024-01-03 17:27:35 +0100

    revert to md table

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -1,11 +1,10 @@
 import os
 import json
 
-root_readme_path = 'readme.md'  # Path to the root README file
+root_readme_path = 'readme.md' 
 meta_data_start_tag = '<!---metadata'
 meta_data_end_tag = '/metadata--->'
 
-# Function to parse JSON metadata from a readme.md file
 def parse_readme_metadata(readme_path):
     with open(readme_path, 'r') as file:
         content = file.read()
@@ -19,27 +18,21 @@ def parse_readme_metadata(readme_path):
 
 def generate_table_row(metadata, level, path):
     tabs = ('&nbsp;' * (level-1)*2)
-    row = f"<td> {metadata['type']} </td><td> { tabs}<a href='{path}'>{metadata['title']}</a> </td><td> {metadata['take']} </td><td> {metadata['status']} </td><td> {metadata['progress']}% </td><td> {', '.join(metadata['tags']) if 'tags' in metadata else ''} </td>"
+    row = f"| {metadata['type']} | { tabs}[{metadata['title']}]({path}) | {metadata['take']} | {metadata['status']} | {metadata['progress']}% | {', '.join(metadata['tags']) if 'tags' in metadata else ''} |\n"
     return row
 
-def generate_html_table_content():
-    html_table_content = "<table><thead><tr><th>Type</th><th>Title</th><th>Take</th><th>Status</th><th>Progress</th><th>Tags</th></tr></thead><tbody>"
+def generate_table_content():
+    table_content = "| Type | Title | Take | Status | Progress | Tags |\n|------|-------|------|--------|----------|------|\n"
     for root, dirs, files in os.walk('.'):
         if 'readme.md' in files:
             readme_path = os.path.join(root, 'readme.md')
             metadata = parse_readme_metadata(readme_path)
             if metadata:
                 level = len([a for a in root.split(os.sep) if a == "e"])
-                row = generate_table_row(metadata, level, readme_path)
-                # Apply conditional styling based on metadata (for example, if status is 'complete')
-                if metadata.get('type') == 'epic':
-                    html_table_content += f'<tr style="color: green;">{row}</tr>'
-                else:
-                    html_table_content += f'<tr>{row}</tr>'
-    html_table_content += "</tbody></table>"
-    return html_table_content
+                table_content += generate_table_row(metadata, level, readme_path)
+    return table_content
 
-html_table_content = generate_html_table_content()
+html_table_content = generate_table_content()
 
 with open('readme.md', 'r+') as root_readme:
     content = root_readme.read()
@@ -50,4 +43,4 @@ with open('readme.md', 'r+') as root_readme:
     root_readme.write(updated_content)
     root_readme.truncate()
 
-print("Root README updated successfully with HTML tree!")
+print("Root README updated successfully with the table!")
